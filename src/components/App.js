49ca9c4b6137e87376e8570de2c28868bff6f1ea
Hernán Melia <hernanmelia@me.com>
2020-12-12 17:35:22 -0300

    Configured babel-parser in ESLint.

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const App = () => (<>Reddit Top 50</>);
+const App = () => <>Reddit Top 50</>;
 
 ReactDOM.render(<App />, document.getElementById("reddit-top-50-app"));
