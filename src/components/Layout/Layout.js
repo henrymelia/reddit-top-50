dc5f65c77ca48b6a961203c50993de47f6db42c8
Hernán Melia <hernanmelia@me.com>
2020-12-20 19:45:10 -0300

    Created the Layout component.

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import classNames from "classnames";
+import SideBar from "../Sidebar";
+import MainContainer from "../MainContainer";
+import { setReadedPost } from "../../actions/posts-actions";
+
+import "./Layout.scss";
+
+const Layout = () => {
+  const [shouldDisplaySidebar, setShouldDisplaySidebar] = useState(true);
+  const selectedPost = useSelector((state) => state.posts.selectedPost);
+  const dispatch = useDispatch();
+
+  return (
+    <>
+      <SideBar
+        className={classNames({
+          sidebar: true,
+          "is-collapsed": !shouldDisplaySidebar,
+        })}
+        onHideSidebar={() => setShouldDisplaySidebar(false)}
+      />
+      <MainContainer
+        className={classNames({
+          content: true,
+          "is-full-width": !shouldDisplaySidebar,
+        })}
+        post={selectedPost}
+        isSidebarVisible={shouldDisplaySidebar}
+        onShowSidebar={() => setShouldDisplaySidebar(true)}
+        markPostAsReaded={(post) => {
+          dispatch(setReadedPost(post));
+        }}
+      />
+    </>
+  );
+};
+
+export default Layout;
