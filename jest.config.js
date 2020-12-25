48209cc045c4b0ab398e4caf3cb85b25b638af3f
Hernán Melia <hernanmelia@me.com>
2020-12-25 20:55:58 -0300

    Created a Jest config file to contain global mocks. Added jest-fetch-mock as dependency.

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,5 @@ module.exports = {
     "\\.(css|scss)$": "identity-obj-proxy",
   },
   testMatch: ["<rootDir>/src/**/?(*.)+(spec|test).[jt]s?(x)"],
+  setupFiles: ["<rootDir>/test/testSetup.js"],
 };
