32d6efe098b535000e27ed9c9ffd160e1374e9ec
Hernán Melia <hernanmelia@me.com>
2020-12-25 20:29:33 -0300

    Fixed a couple of testing issues.

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,6 @@
 module.exports = {
+  moduleNameMapper: {
+    "\\.(css|scss)$": "identity-obj-proxy",
+  },
   testMatch: ["<rootDir>/src/**/?(*.)+(spec|test).[jt]s?(x)"],
 };
--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -3,9 +3,24 @@ import { render } from "@testing-library/react";
 
 import App from "./App";
 
+Object.defineProperty(window, "matchMedia", {
+  writable: true,
+  value: jest.fn().mockImplementation((query) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(), // deprecated
+    removeListener: jest.fn(), // deprecated
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+  })),
+});
+
 describe("App Component", () => {
   it("renders", () => {
     const { getByText } = render(<App />);
-    expect(getByText("Reddit Top 50").textContent).toBe("Reddit Top 50");
+    expect(getByText("Top Posts").textContent).toBe("Top Posts");
+    expect(getByText("👈 Pick a post").textContent).toBe("👈 Pick a post");
   });
 });
