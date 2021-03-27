d9de8966229010ca7c620724e59798694d71a3d6
Hernán Melia <hernanmelia@me.com>
2021-03-27 01:15:18 -0300

    Changed the id of the element in which the app will be bootstrapped.

--- src/render-app.js
+++ src/render-app.js
@@ -2,4 +2,4 @@ import React from "react";
 import { render } from "react-dom";
 import App from "./components/App";
 
-render(<App />, document.getElementById("reddit-top-50-app"));
+render(<App />, document.getElementById("reddit-top-50"));
